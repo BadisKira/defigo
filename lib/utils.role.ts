4639153d3d9429d6faa6f93d07b5191f071724b9
Badis Hammadache <badisgithub@gmail.com>
2025-06-03 15:54:35 +0200

    add admin page v1

---
+++ lib/utils.role.ts
@@ -0,0 +1,6 @@
+import { Roles } from '@/types/globals'
+import { auth } from '@clerk/nextjs/server'
+export const checkRole = async (role: Roles) => {
+  const { sessionClaims } = await auth()
+  return sessionClaims?.metadata.role === role
+}
